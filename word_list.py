14b397c4c9f73eb622a6951cd828b19e9f5e099e
kaspatcho <nosilaw7777@gmail.com>
2021-08-26 00:06:38 -0300

    putting wordlist in other file

---
+++ word_list.py
@@ -0,0 +1,33 @@
+WORDS = {
+    'país' : [
+        'China', 'India', 'Estados Unidos', 'Indonesia', 'Paquistao', 'Brasil', 'Nigeria', 'Bangladesh', 'Russia',
+        'Mexico', 'Japao', 'Etiopia', 'Filipinas', 'Egito', 'Vietname', 'Republica Democratica do Congo', 'Turquia',
+        'Ira', 'Alemanha', 'Tailandia', 'Reino Unido', 'França', 'Italia', 'Tanzania', 'africa do Sul', 'Myanmar', 'Quenia',
+        'Coreia do Sul', 'Colombia', 'Espanha', 'Uganda', 'Argentina', 'Argelia', 'Sudao', 'Ucrania', 'Iraque', 'Afeganistao',
+        'Polonia', 'Canada', 'Marrocos', 'Arabia Saudita', 'Uzbequistao', 'Peru', 'Angola', 'Malasia', 'Moçambique', 'Gana',
+        'Republica Centro-Africana', 'Nova Zelandia', 'Mauritania', 'Panama', 'Kuwait', 'Croacia', 'Georgia', 'Eritreia',
+        'Uruguai', 'Bosnia e Herzegovina', 'Mongolia', 'Armenia', 'Jamaica', 'Catar', 'Albania', 'Porto Rico', 'Lituania',
+        'Moldavia', 'Namibia', 'Gambia', 'Botswana', 'Gabao', 'Lesoto', 'Macedonia do Norte', 'Eslovenia', 'Guine-Bissau',
+        'Letonia', 'Kosovo', 'Bahrein', 'Guine Equatorial', 'Trinidad e Tobago', 'Estonia', 'Timor-Leste', 'Mauricia', 'Eswatini',
+        'Granada', 'Sao Vicente e Granadinas', 'Jersey', 'Aruba', 'Tonga', 'U S  Ilhas Virgens', 'Seicheles', 'Antigua e Barbuda'
+    ],
+    'nome' : [
+        'Djaniny', 'Dinis', 'Alao', 'Cheng', 'Ilie', 'Aaliyah', 'Ibaadali', 'Claudio', 'Hermano', 'Enzu', 'Dailson',
+        'Django', 'Arham', 'Johny', 'Cademo', 'Ashan', 'Adja', 'Cooper', 'Ines', 'Havya', 'Aryana', 'Geoffrey',
+        'Annalisa', 'Ayana', 'Habacuque', 'Charbel', 'Gislane', 'Djeisen', 'Esenia', 'Estebane', 'Anselmo', 'Asvi',
+        'Christopher', 'Amber', 'Halana', 'andria', 'Angeline', 'Harkirat', 'Dario', 'Oswaldo', 'Doralice', 'Dieudonne',
+        'Dennys', 'Iryna', 'Dien', 'Harleen', 'Aderito', 'Emmanuela', 'Adla', 'Allister', 'Hercio', 'Cleopatra',
+        'Abdulwahab', 'Ennzo', 'Eudes', 'anghel', 'Georgeta', 'Hamda', 'Dhruv', 'Gênesis', 'Esteban', 'Agandeep', 'Gilcelio',
+        'Constança', 'Harbin', 'Aihra', 'Bogdan', 'Cizia', 'Bryant', 'Agampreet', 'Christiaan', 'Angelino', 'Jayden', 'Golias',
+        'Elona', 'Andersa', 'Greg', 'Alexandru', 'Aksh', 'Helao', 'Elias', 'Ananias', 'Catia', 'Ezairon'
+    ],
+    'comida' : [
+        'Amoras', 'arroz', 'abacate', 'ameixa', 'atum', 'alface', 'Baiao de dois', 'beterraba', 'batata', 'bolo',
+        'banana', 'Cuscuz', 'camarao', 'carne', 'cordeiro', 'cereja', 'Damasco', 'dobradinha', 'danone', 'doce de leite',
+        'Espaguete', 'empada', 'escondidinho', 'Feijao', 'farofa', 'feijoada', 'framboesa', 'figo', 'Gengibre', 'gorgonzola',
+        'groselha', 'goiaba', 'goiabada', 'Iogurte', 'inhame', 'Jabuticaba', 'jaca', 'Kiwi', 'Laranja', 'lasanha', 'limao',
+        'Melao', 'melado', 'mel', 'macarrao', 'mostarda', 'maionese', 'manga', 'maca', 'Nozes', 'nachos', 'Ovos', 'omelete',
+        'Peixe', 'pepino', 'pimentao', 'pate', 'pimenta', 'Queijo', 'Rosbife', 'rabada', 'rabanada', 'requeijao', 'Sopa',
+        'soja', 'Tofu', 'tomate', 'Uvas', 'Vagem'
+    ]
+}
